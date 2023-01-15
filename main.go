5553e8375342fb639b95d1ac4139dc59774e7e05
kissejau <kissejau@mail.ru>
2023-01-15 13:15:12 +0300

    switched numbering

--- main.go
+++ main.go
@@ -8,10 +8,8 @@ import (
 )
 
 var (
-	SIDE_INDENT int = 2
-	NUM_INDENT  int = 3
-	CUR_ROW     int = 0
-	CUR_COL     int = SIDE_INDENT + NUM_INDENT + 1 // first row index (1)
+	CUR_ROW int = 0
+	CUR_COL int = 0 // first row index (1)
 
 	BUFFER map[int][]byte
 )
@@ -21,10 +19,10 @@ func localizeData() {
 
 	for row := 0; row < h; row++ {
 		BUFFER[row] = make([]byte, w)
-		numberingBufferRows(row)
 		for col := 0; col < w; col++ {
 		}
 	}
+	additionalInfo(BUFFER[h-1])
 }
 
 func display() {
@@ -38,6 +36,7 @@ mainloop:
 				break mainloop
 			}
 			term.SetCursor(CUR_COL, CUR_ROW)
+			additionalInfo(BUFFER[len(BUFFER)-1])
 			draw()
 		case term.EventResize:
 			draw()
@@ -52,7 +51,7 @@ func draw() {
 
 	for row := 0; row < h; row++ {
 		for col := 0; col < w; col++ {
-			term.SetCell(col, row, rune(BUFFER[row][col]), term.AttrBlink, term.AttrBlink)
+			term.SetCell(col, row, rune(BUFFER[row][col]), term.ColorGreen, term.ColorDarkGray)
 		}
 	}
 	term.Flush()
@@ -85,9 +84,6 @@ func keysHandler(e term.Event) bool {
 		leftShift()
 		recalcCursorPos(-1, 0)
 
-	// case term.KeyEnd:
-	// 	goEnd()
-
 	default:
 		if e.Ch != 0 {
 			printSymbol(e.Ch)
@@ -96,12 +92,6 @@ func keysHandler(e term.Event) bool {
 
 	return false
 }
-
-// func goEnd() {
-// 	w, _ := term.Size()
-
-// 	CUR_COL = w - 1
-// }
 
 func leftShift() {
 	var temp byte = BUFFER[CUR_ROW][len(BUFFER[CUR_ROW])-1]
@@ -132,14 +122,16 @@ func printSymbol(ch rune) {
 func recalcCursorPos(col, row int) {
 	w, _ := term.Size()
 	if col != 0 {
-		if CUR_COL+col < NUM_INDENT+SIDE_INDENT+len(strconv.FormatInt(int64(CUR_ROW), 10)) && CUR_ROW > 0 {
-			CUR_ROW--
-			CUR_COL = w - 1
-		} else if CUR_COL+col >= w {
-			CUR_ROW++
-			CUR_COL = NUM_INDENT + SIDE_INDENT + len(strconv.FormatInt(int64(CUR_ROW), 10))
-		} else if CUR_COL+col >= NUM_INDENT+SIDE_INDENT+len(strconv.FormatInt(int64(CUR_ROW), 10)) {
-			CUR_COL += col
+		if CUR_ROW > 0 || CUR_COL+col >= 0 {
+			if CUR_COL+col < 0 {
+				CUR_COL = w - 1
+				CUR_ROW--
+			} else if CUR_COL+col >= w {
+				CUR_COL = 0
+				CUR_ROW++
+			} else {
+				CUR_COL += col
+			}
 		}
 	} else {
 		if CUR_ROW+row >= 0 {
@@ -148,11 +140,13 @@ func recalcCursorPos(col, row int) {
 	}
 }
 
-func numberingBufferRows(rowIndex int) {
-	colIndex := 0
-	for _, e := range strconv.FormatInt(int64(rowIndex), 10) {
-		BUFFER[rowIndex][colIndex+SIDE_INDENT] = byte(e)
-		colIndex++
+func additionalInfo(arr []byte) {
+	for i, _ := range arr {
+		arr[i] = 0
+	}
+	out := strconv.FormatInt(int64(CUR_ROW), 10) + "," + strconv.FormatInt(int64(CUR_COL), 10)
+	for i, _ := range out {
+		arr[len(arr)/2+i] = out[i]
 	}
 }
 
